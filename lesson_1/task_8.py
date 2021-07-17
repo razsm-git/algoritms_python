70400c9867154532fae9bd0a6785b0cddd4267bf
razsm-git <76174833+razsm-git@users.noreply.github.com>
2021-07-17 16:24:06 +0300

    Delete lesson_1 directory

--- lesson_1/task_8.py
+++
@@ -1,6 +0,0 @@
-year = int(input("Введите год для проверки: "))
-
-if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    print(f"{year} год високосный")
-else:
-    print(f"{year} год не високосный")
